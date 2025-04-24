f8219511f96b919b80aca94129f68eb1e8141f95
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-24 14:53:53 +0100

    baseline

--- model/loss.py
+++ model/loss.py
@@ -56,9 +56,9 @@ def covariance_matching_loss(expert_outputs: torch.Tensor, eps: float = 1e-6) ->
 
 
 def calculate_lambda_max_loss(x):   
-    # (batch, K, dim)
-    x = F.normalize(x, p=2, dim=-1)  # now normalizes each dim-vector
-    A = x.permute(2, 1, 0).contiguous()  # (d, n, batch)
+    # x is shape (dim, batch, K)
+    A = x.permute(1, 2, 0).contiguous() 
+    x = F.normalize(x, p=2, dim=0)  # now normalizes each dim-vector
     print(A.shape)
     eps = 1e-6
 
@@ -71,7 +71,7 @@ def calculate_lambda_max_loss(x):
         
     r_diag = R.abs().diagonal(dim1=-2, dim2=-1)           # (E, min(d,B))
     k      = (r_diag > eps).sum(dim=1)   
-    
+
     for i, ki in enumerate(k):
         print(f"expert_{i}_empirical_rank", ki.item())
     cols   = torch.arange(Q.size(-1), device=Q.device)    # (d,)
